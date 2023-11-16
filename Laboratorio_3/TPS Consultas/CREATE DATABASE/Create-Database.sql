09096e9272cdd706dea9d6a37c2ce3ae61e904d0
Alex Gustavo Welz <93561231+alexwelz@users.noreply.github.com>
2023-11-16 01:46:38 -0300

    Update Create-Database.sql

--- Laboratorio_3/TPS Consultas/CREATE DATABASE/Create-Database.sql
+++ Laboratorio_3/TPS Consultas/CREATE DATABASE/Create-Database.sql
@@ -136,8 +136,81 @@ add CONSTRAINT FK_Recetas_UnidadMedida FOREIGN key (IDUnidadMedida) REFERENCES U
 go 
 Alter Table Recetas
 add CONSTRAINT CHK_Cantidad CHECK (Cantidad > 0)
+
+	
 ----------------------------------------------------------------------------------------------------------------
 
+CREATE DATABASE Consultas
+GO
+USE Consultas 
+GO
+CREATE TABLE Paises(
+    ID_Pais int not null primary key identity(1,1),
+    Pais nvarchar(50) not null
+)
+GO
+CREATE TABLE Provincias(
+    ID_Provincia int not null primary key identity(1,1),
+    Provincia nvarchar(50) not null,
+    ID_Pais int not null foreign key references Paises(ID_Pais)
+)
+GO 
+CREATE TABLE Ciudades(
+    ID_Ciudad int not null primary key identity (1,1),
+    Ciudad nvarchar(50) not null,
+    ID_Provincia int not null foreign key references Provincias(ID_Provincia)
+)
+GO
+CREATE TABLE Alumnos(
+    Legajo int not null primary key,
+    Apellido nvarchar(100) not null,
+    Nombre nvarchar(100) not null,
+    Fecha_Nacimiento smalldatetime not null,
+    Direccion nvarchar(100) not null,
+    ID_CiudadNacimento int not null foreign key references Ciudades(ID_Ciudad),
+    Telefono varchar(20) null,
+    Email varchar(50) null,
+    Sexo char(1) null
+)
+
+
+
+INSERT INTO Paises (Pais)
+VALUES ('Argentina'), ('Estados Unidos'), ('España'), ('Brasil'), ('México'), ('Francia');
+
+INSERT INTO Provincias (Provincia, ID_Pais)
+VALUES
+    ('Buenos Aires', 1),
+    ('California', 2),
+    ('Madrid', 3),
+    ('Sao Paulo', 4),
+    ('Jalisco', 5),
+    ('Île-de-France', 6);
+
+
+INSERT INTO Ciudades (Ciudad, ID_Provincia)
+VALUES
+    ('CABA', 1),
+    ('Los Angeles', 2),
+    ('Madrid Capital', 3),
+    ('Sao Paulo Capital', 4),
+    ('Guadalajara', 5),
+    ('París', 6);
+
+
+INSERT INTO Alumnos (Legajo, Apellido, Nombre, Fecha_Nacimiento, Direccion, ID_CiudadNacimento, Telefono, Email, Sexo)
+VALUES
+    (1001, 'Perez', 'Juan', '2000-05-15', 'Calle 123, CABA', 1, '123-456-7890', 'juan@example.com', 'M'),
+    (1002, 'Smith', 'Emma', '1999-08-22', '123 Main St, Los Angeles', 2, '555-123-4567', 'emma@example.com', 'F'),
+    (1003, 'García', 'Luis', '2001-02-10', 'Calle Mayor 45, Madrid Capital', 3, '123-789-4560', 'luis@example.com', 'M'),
+    (1004, 'Ferreira', 'Maria', '2000-07-10', 'Rua 123, Sao Paulo', 4, '123-555-7890', 'maria@example.com', 'F'),
+    (1005, 'López', 'Carlos', '1998-04-18', 'Avenida Principal 456, Guadalajara', 5, '333-789-1234', 'carlos@example.com', 'M'),
+    (1006, 'Dubois', 'Sophie', '2002-01-25', 'Rue de la Paix 789, París', 6, '555-999-8888', 'sophie@example.com', 'F');
+
+
+
+----------------------------------------------------------------------------------------------------------------
+	
 CREATE DATABASE Punto1_ExamenIntegrador
 GO
 USE Punto1_ExamenIntegrador
